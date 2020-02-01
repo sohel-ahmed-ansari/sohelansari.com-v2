03eb248c45607505f13728cb37c566cb594977e3
sohel-ahmed-ansari <sohel.ahmed.ansari@gmail.com>
2020-02-01 16:17:01 +0400

    React-webpack boiler plate ready

---
+++ src/components/app/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.less';
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="app__title">This is a React-Webpack boiler plate!</div>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+const config = {
+  entry: ['@babel/polyfill', './src/index.js'],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: "[name].js"
+  },
+  plugins: [htmlPlugin],
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader'
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        loader: "file-loader",
+        options: { name: '/static/[name].[ext]' }
+      }
+    ]
+  }
+};
+
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+    config.devtool = 'eval-source-map';
+  }
+  return config;
+};
