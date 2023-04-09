8b5415f4d5931e581690939de608b5d76fffc63b
Sohel Ahmed Ansari <sohel.ansari@forto.com>
2023-04-10 01:17:35 +0200

    Update node version and upgrade all packages to latest

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,56 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const path = require('path');
+const path = require("path");
 const htmlPlugin = new HtmlWebPackPlugin({
   template: "./src/index.html",
-  filename: "./index.html"
+  filename: "./index.html",
 });
-const CopyPlugin = require('copy-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
 
 const config = {
-  entry: ['@babel/polyfill', './src/index.js'],
+  entry: ["@babel/polyfill", "./src/index.js"],
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: "[name].js"
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
   },
   plugins: [
     htmlPlugin,
     new CopyPlugin({
-      patterns: [
-        { from: 'src/assets', to: 'assets' }
-      ]
-    })
+      patterns: [{ from: "src/assets", to: "assets" }],
+    }),
+    new ESLintPlugin(),
   ],
   module: {
     rules: [
       {
-        enforce: 'pre',
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader'
-      },
-      {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: "babel-loader",
+        },
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif|ttf|otf)$/,
         loader: "file-loader",
-        options: { name: '/static/[name].[ext]' }
-      }
-    ]
-  }
+        options: { name: "/static/[name].[ext]" },
+      },
+    ],
+  },
 };
 
 styleLoaderRules = {
   dev: {
     test: /\.less$/,
     use: [
-      'style-loader',
+      "style-loader",
       {
-        loader: 'css-loader',
+        loader: "css-loader",
         options: {
           sourceMap: true,
         },
       },
       {
-        loader: 'less-loader',
+        loader: "less-loader",
         options: {
           sourceMap: true,
         },
@@ -65,17 +59,13 @@ styleLoaderRules = {
   },
   prod: {
     test: /\.less$/,
-    use: [
-      'style-loader',
-      'css-loader',
-      'less-loader'
-    ]
-  }
-}
+    use: ["style-loader", "css-loader", "less-loader"],
+  },
+};
 
 module.exports = (env, argv) => {
-  if (argv.mode === 'development') {
-    config.devtool = 'eval-source-map';
+  if (argv.mode === "development") {
+    config.devtool = "eval-source-map";
     config.module.rules.push(styleLoaderRules.dev);
   } else {
     config.module.rules.push(styleLoaderRules.prod);
